3cd708b4da7b248aa2a36bf0d2bc52ca44bce413
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-08-06 12:14:09 -0300

    add component MovieRow

---
+++ src/components/MovieRow/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './styles.module.css'
+import { ItemsType } from '../../interfaces'
+
+const rowList = ({ title, items }: {
+  title: string,
+  items: ItemsType,
+}): JSX.Element => (
+  <div className={styles.movieRow}>
+    <h2>{title}</h2>
+    <div className={styles.listArea}>
+      <div className={styles.list}>
+        {items.results.length > 0 && items.results
+          .filter((item) => item.poster_path !== null)
+          .map((item) => (
+            <div className={styles.item} key={item.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                alt={item.name}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  </div>
+)
+
+export default rowList
