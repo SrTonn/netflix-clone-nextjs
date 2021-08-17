14fd6b016a56b6daac620ffaa28eb725694e47af
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-08-16 21:06:36 -0300

    add dynamic api router

---
+++ src/pages/api/tmdb/MovieInfo/[...slug].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import basicFetch from '../../../../functions/functions'
+
+const API_KEY = process.env.NEXT_PUBLIC_APP_TMDB_API_KEY
+const API_BASE = 'https://api.themoviedb.org/3'
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  let info = {}
+  const [type, movieId] = req.query.slug
+
+  if (movieId) {
+    switch (type) {
+    case 'movei':
+      info = await basicFetch(
+        `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`,
+        API_BASE,
+        API_KEY,
+      )
+      break
+    case 'tv':
+      info = await basicFetch(
+        `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`,
+        API_BASE,
+        API_KEY,
+      )
+      break
+    default:
+      info = null
+      break
+    }
+  }
+  res.status(200).json(info)
+}
