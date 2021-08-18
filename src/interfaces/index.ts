21d7562ef2af8639fcb53873949f5d29b308c3fc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-08-17 23:18:46 -0300

    add infos to FeaturedMovieProps type

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,9 +2,9 @@ export type MovieRowProps = {
   id: number,
   results: Array<{
     id: number,
-     name: string,
-     title: string,
-     poster_path: string
+    name: string,
+    title: string,
+    poster_path: string
   }>
 }
 
@@ -13,4 +13,11 @@ export type FeaturedMovieProps = {
   original_name: string,
   backdrop_path: string,
   overview: string,
+  first_air_date: string,
+  vote_average: number,
+  number_of_seasons: number,
+  genres: Array<{
+    id: number,
+    name: string
+  }>,
 }
